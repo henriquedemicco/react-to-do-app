dc5463efdf9f43963ccc9fe8b7f8d3a951858e7d
Miccao <henriquedemicco@hotmail.com>
2021-10-06 18:58:00 -0300

    Primeiro commit

---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,9 @@
+import * as C from './style';
+
+export const Icone = () => {
+    return (
+        <C.Icone>
+            <img src="/imagens/to-do-list.png" />
+        </C.Icone>
+    )
+}
---
+++ src/components/Icon/style.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const Icone = styled.div`
+    padding: 5px;
+`;
